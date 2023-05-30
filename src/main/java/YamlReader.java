44f994247b67dceada8430d6d33ac053eae1b7f7
lluckyboi <1598273095@qq.com>
2023-05-30 11:46:19 +0800

    feat:通过yaml配置

---
+++ src/main/java/YamlReader.java
@@ -0,0 +1,18 @@
+import org.ho.yaml.Yaml;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+public class YamlReader {
+    public int Idx;
+    // 读取yaml配置文件 获取单词索引
+    public  YamlReader() throws FileNotFoundException {
+        File dumpFile=new File(Objects.requireNonNull(this.getClass().getClassLoader().getResource("")).getPath() + "/conf.yaml");
+        Map mp = Yaml.loadType(dumpFile, HashMap.class);
+        System.out.println(mp.get("IDX").hashCode());
+        Idx =mp.get("IDX").hashCode();
+    }
+}
